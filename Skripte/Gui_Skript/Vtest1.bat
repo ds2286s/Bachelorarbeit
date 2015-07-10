281d7ea714f919a11ad2de2241449517c2311d6b
ds2286s <dennis.stolp@googlemail.com>
2015-07-10 11:54:43 +0200

    Batch hinzugefuegt

--- Skripte/Gui_Skript/Vtest1.bat
+++ Skripte/Gui_Skript/Vtest1.bat
@@ -1,2 +1,2 @@
-Ventiltest1.au3
-..\Python_Skript\SkriptFile.py COM18 Ventiltest1.txt
+..\..\Build_Testsketches\workspace\Skripte\Gui_Skript\Ventiltest1.au3
+..\..\Build_Testsketches\workspace\Skripte\Python_Skript\SkriptFile.py COM18 Ventiltest1.txt
